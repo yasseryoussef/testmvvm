7503f77802a07530c9a3822c30f5fdd106e9e3ce
yasseryoussef <swap-solutions@hotmail.com>
2022-12-07 23:18:13 +0200

    Add project files.

---
+++ MauiApp2/testvm.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp2
+{
+    public partial class testvm: ObservableObject
+    {
+        [RelayCommand]
+        public async  void add_data()
+        {
+
+        }
+    }
+}
